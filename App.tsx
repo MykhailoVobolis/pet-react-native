f3181e865428337830c54706fb898d2d036c8cfe
MykhailoVobolis <mvobolis@gmail.com>
2024-09-22 00:33:18 +0300

    add debugger

--- App.tsx
+++ App.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
-import {SafeAreaView} from 'react-native';
+import React, {useEffect} from 'react';
+import {DevSettings, NativeModules, SafeAreaView} from 'react-native';
 import LoginPage from './src/screen/Auth/Login';
 
 function App(): React.JSX.Element {
+  // Налаштування дебвг меню у react-native-debugger
+  useEffect(() => {
+    if (__DEV__) {
+      // Додавання меню старту дебагера
+      DevSettings.addMenuItem('Debugging With debugger', () => {
+        NativeModules.DevSettings.setIsDebuggingRemotely(true);
+      });
+      // Додавання меню зупинки дебагера
+      DevSettings.addMenuItem('Stop Debugging With debugger', () => {
+        NativeModules.DevSettings.setIsDebuggingRemotely(false);
+      });
+    }
+    console.log('Info', {age: 25, array: [1, 2, 3, 5, {name: 'Mykhailo'}]});
+    fetch('https://google.com');
+  }, []);
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <LoginPage />
