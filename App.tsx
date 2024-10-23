bdba15c853d895a5185226fc51759949642b604f
MykhailoVobolis <mvobolis@gmail.com>
2024-10-23 17:36:30 +0300

    added pets collection

--- App.tsx
+++ App.tsx
@@ -2,8 +2,27 @@ import React, {useEffect} from 'react';
 import {DevSettings, NativeModules, SafeAreaView} from 'react-native';
 import RootNavigation from './src/navigation';
 import 'react-native-gesture-handler';
+// import {firebase} from '@react-native-firebase/firestore';
+// import {animals} from './animals_list';
 
 function App(): React.JSX.Element {
+  // const db = firebase.firestore();
+
+  // const uploadAnimalsToFirestore = async animals => {
+  //   const collectionRef = db.collection('animals');
+
+  //   for (const animal of animals) {
+  //     await collectionRef
+  //       .add(animal)
+  //       .then(docRef => {
+  //         console.log(`Document written with ID: ${docRef.id}`);
+  //       })
+  //       .catch(error => {
+  //         console.error('Error adding document: ', error);
+  //       });
+  //   }
+  // };
+
   // Налаштування дебвг меню у react-native-debugger
   useEffect(() => {
     if (__DEV__) {
@@ -16,6 +35,7 @@ function App(): React.JSX.Element {
         NativeModules.DevSettings.setIsDebuggingRemotely(false);
       });
     }
+    // uploadAnimalsToFirestore(animals);
   }, []);
 
   return (
--- src/screen/Home/index.tsx
+++ src/screen/Home/index.tsx
@@ -1,17 +1,47 @@
 import {StackNavigationProp} from '@react-navigation/stack';
-import {Text, TouchableOpacity} from 'react-native';
+import {View} from 'react-native';
 import {RootStackNavigation} from '../../navigation/types';
 import {useNavigation} from '@react-navigation/native';
+import firestore from '@react-native-firebase/firestore';
+import {useEffect, useState} from 'react';
+import PetsList from './components/PetsList';
+
+export interface IPets {
+  age: number;
+  color: string;
+  description: string;
+  images: string[];
+  isDog: boolean;
+  isVaccinated: boolean;
+  location: string;
+  name: string;
+  sex: string;
+  type: string;
+  timeStamp: number;
+  size: 'big' | 'medium' | 'small';
+}
 
 export default function Home() {
   const navigation = useNavigation<StackNavigationProp<RootStackNavigation>>();
+  const [pets, setPets] = useState<IPets[]>([]);
+
+  const getPets = async () => {
+    try {
+      const result = await firestore().collection('animals').get();
+      const temp: IPets[] = result.docs.map(i => i.data()) as IPets[];
+      setPets(temp);
+    } catch (error) {
+      console.log('Error', error);
+    }
+  };
+
+  useEffect(() => {
+    getPets();
+  }, []);
 
   return (
-    <TouchableOpacity
-      onPress={() => {
-        navigation.goBack();
-      }}>
-      <Text>Hello user</Text>
-    </TouchableOpacity>
+    <View style={{flex: 1}}>
+      <PetsList pets={pets} />
+    </View>
   );
 }
